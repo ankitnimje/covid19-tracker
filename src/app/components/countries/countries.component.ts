fdbb2f579019173587ad8a56dccc1cbd0c887c57
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-10-29 00:27:57 -0700

    Fix the bug which prevents updating the chart

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,88 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { DateWiseData } from 'src/app/models/date-wise-data';
-// import { GlobalDataSummary } from 'src/app/models/global-data';
-// import { DataServiceService } from 'src/app/services/data-service.service';
-// import { merge } from 'rxjs';
-// import { map } from 'rxjs/operators';
-
-// @Component({
-//   selector: 'app-countries',
-//   templateUrl: './countries.component.html',
-//   styleUrls: ['./countries.component.css']
-// })
-// export class CountriesComponent implements OnInit {
-
-//   data : GlobalDataSummary[];
-//   countries : string[] = [];
-//   totalConfirmed = 0;
-//   totalActive = 0;
-//   totalDeaths = 0;
-//   totalRecovered = 0;
-//   selectedCountryData : DateWiseData[];
-//   dateWiseData ;
-//   dataTable = [];
-
-//   chart = {
-//     LineChart : "LineChart",
-//     height: 500,
-//     options: {
-//       animation: {
-//         duration: 1000,
-//         easing: 'out'
-//       },
-//       is3D: true
-//     }
-//   }
-
-//   constructor(private service : DataServiceService) { }
-
-//   ngOnInit(): void {
-
-//     this.service.getDateWiseData().subscribe(
-//       (result) => {
-//         this.dateWiseData = result;
-//         this.updateChart();
-//         // console.log(result);
-
-//       }
-//     );
-
-//     this.service.getGlobalData().subscribe(result => {
-//       this.data = result;
-//       this.data.forEach(cs => {
-//         this.countries.push(cs.country);
-//       })
-//     })
-//   }
-
-//   updateChart() {
-//     // this.dataTable = [];
-//     this.dataTable.push(["Date" , 'Cases'])
-//     this.selectedCountryData.forEach(cs=>{
-//       this.dataTable.push([cs.date , cs.cases])
-//     })
-//   }
-
-//   updateValues(country : string) {
-//     console.log(country);
-//     this.data.forEach(cs => {
-//       if(cs.country == country) {
-//         this.totalActive = cs.active;
-//         this.totalConfirmed = cs.confirmed;
-//         this.totalRecovered = cs.recovered;
-//         this.totalDeaths = cs.deaths;
-//       }
-//     })
-
-//     this.selectedCountryData = this.dateWiseData[country];
-//     console.log(this.selectedCountryData);
-//     this.updateChart();
-//   }
-
-// }
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { GlobalDataSummary } from 'src/app/models/global-data';
@@ -149,7 +64,7 @@ export class CountriesComponent implements OnInit {
   }
 
   updateChart(){
-    // let dataTable = [];
+    this.dataTable = [];
     // this.dataTable.push(['Date' , 'Cases'])
     // console.log("updateChart called");
 
